fc6dc136d8549744dec86227d5002d23dd835903
Mr. Matt <matt@composition9.com>
2015-01-16 11:33:41 -0700

    wrap calls to scout library in ruby_blocks

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,28 +19,41 @@ user node[:scout][:user] do
   only_if do node[:scout][:user] != 'root' end
 end.run_action(:create)
 
-# install scout agent gem
-if node[:scout][:version]
-  Scout.install_gem(node, ["scout", "--version", "#{node[:scout][:version]}"])
-else
-  Scout.install_gem(node, ["scout"])
+# wrap calls to the Scout library in ruby_block
+# this prevents code that looks for binaries from being executed before the run_list
+ruby_block "install the scout agent gem" do
+  block do
+    # install scout agent gem
+    if node[:scout][:version]
+      Scout.install_gem(node, ["scout", "--version", "#{node[:scout][:version]}"])
+    else
+      Scout.install_gem(node, ["scout"])
+    end
+  end
 end
 
 if node[:scout][:key]
-  scout_bin = Scout.scout_binary(node)
-  hostname_attr = node[:scout][:hostname] ? %{ --hostname "#{node[:scout][:hostname]}"} : ""
-  name_attr = node[:scout][:name] ? %{ --name "#{node[:scout][:name]}"} : ""
-  server_attr = node[:scout][:server] ? %{ --server "#{node[:scout][:server]}"} : ""
-  roles_attr = node[:scout][:roles] ? %{ --roles "#{node[:scout][:roles].map(&:to_s).join(',')}"} : ""
-  http_proxy_attr = node[:scout][:http_proxy] ? %{ --http-proxy "#{node[:scout][:http_proxy]}"} : ""
-  https_proxy_attr = node[:scout][:https_proxy] ? %{ --https-proxy "#{node[:scout][:https_proxy]}"} : ""
-  environment_attr = node[:scout][:environment] ? %{ --environment "#{node[:scout][:environment]}"} : ""
+  # wrap calls to the Scout library in ruby_block
+  ruby_block "find the scout binary" do
+    block do
+      scout_bin = Scout.scout_binary(node)
+      hostname_attr = node[:scout][:hostname] ? %{ --hostname "#{node[:scout][:hostname]}"} : ""
+      name_attr = node[:scout][:name] ? %{ --name "#{node[:scout][:name]}"} : ""
+      server_attr = node[:scout][:server] ? %{ --server "#{node[:scout][:server]}"} : ""
+      roles_attr = node[:scout][:roles] ? %{ --roles "#{node[:scout][:roles].map(&:to_s).join(',')}"} : ""
+      http_proxy_attr = node[:scout][:http_proxy] ? %{ --http-proxy "#{node[:scout][:http_proxy]}"} : ""
+      https_proxy_attr = node[:scout][:https_proxy] ? %{ --https-proxy "#{node[:scout][:https_proxy]}"} : ""
+      environment_attr = node[:scout][:environment] ? %{ --environment "#{node[:scout][:environment]}"} : ""
 
-  # schedule scout agent to run via cron
-  cron "scout_run" do
-    user node[:scout][:user]
-    command "#{scout_bin} #{node[:scout][:key]}#{hostname_attr}#{name_attr}#{server_attr}#{roles_attr}#{http_proxy_attr}#{https_proxy_attr}#{environment_attr}"
-    only_if do File.exist?(scout_bin) end
+      if(File.exist?(scout_bin))
+        # schedule scout agent to run via cron
+        # need to instantiate the resource manually inside a ruby_block
+        cron = Chef::Resource::Cron.new("scout_run", run_context)
+        cron.user node[:scout][:user]
+        cron.command "#{scout_bin} #{node[:scout][:key]}#{hostname_attr}#{name_attr}#{server_attr}#{roles_attr}#{http_proxy_attr}#{https_proxy_attr}#{environment_attr}"
+        cron.run_action :create
+      end
+    end
   end
 else
   Chef::Log.warn "The agent will not report to scoutapp.com as a key wasn't provided. Provide a [:scout][:key] attribute to complete the install."
@@ -80,7 +93,12 @@ else
 end
 
 (node[:scout][:plugin_gems] || []).each do |gemname|
-  Scout.install_gem(node, [gemname])
+  # wrap calls to the Scout library in ruby_block
+  ruby_block "install a gem" do
+    block do
+      Scout.install_gem(node, [gemname])
+    end
+  end
 end
 
 # Create plugin lookup properties
